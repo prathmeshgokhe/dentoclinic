25ec2bac9242500206263839a4d13174f51691df
Prathmesh Gokhe <pkgokhe1969@gmail.com>
2024-02-16 01:24:19 +0530

    Testimonial Page done

--- components/Testimonial.jsx
+++ components/Testimonial.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import styles from "@/styles/testimonial.module.scss";
+import Image from "next/image";
 
 const Testimonial = () => {
   return (
-    <div>
-      <section class="text-gray-600 body-font px-[100px]">
+    <div className={styles.main}>
+      <section class="text-gray-600 body-font">
         <div class="container px-5 py-24 mx-auto">
           <h1 class="text-3xl font-medium title-font text-gray-900 mb-12 text-center">
             Testimonials
@@ -20,17 +22,88 @@ const Testimonial = () => {
                   <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
                 </svg>
                 <p class="leading-relaxed mb-6">
-                  Synth chartreuse iPhone lomo cray raw denim brunch everyday
-                  carry neutra before they sold out fixie microdosing. Tacos
-                  pinterest fanny pack venmo, post-ironic heirloom try-hard
-                  pabst authentic iceland.
+                  My first visit to Dr. Surbhi Pande was a great experience. She
+                  immediately puts you at ease with her calm and sweet demeanor.
+                  The most impressive part of my visit was that Dr. Surbhi took
+                  the time to show me my problem areas on my x-rays. This helped
+                  me to know why and how to care for my teeth better, thus
+                  motivating me to do so. I never felt rushed. She and her staff
+                  have a special way of making your visit personalized,
+                  comfortable and stress-free. The staff is friendly and very
+                  professional from the moment you walk in and throughout your
+                  appointment.
                 </p>
                 <a class="inline-flex items-center">
+                  <Image
+                    src="/human.svg"
+                    alt="Picture of the author"
+                    width={500}
+                    height={500}
+                    className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"
+                  />
+
                   <span class="flex-grow flex flex-col pl-4">
-                    <span class="title-font font-medium text-gray-900">
-                      Holden Caulfield
+                    <span class="flex items-center">
+                      <svg
+                        fill="currentColor"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        class="w-4 h-4 text-indigo-500"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                      </svg>
+                      <svg
+                        fill="currentColor"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        class="w-4 h-4 text-indigo-500"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                      </svg>
+                      <svg
+                        fill="currentColor"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        class="w-4 h-4 text-indigo-500"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                      </svg>
+                      <svg
+                        fill="currentColor"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        class="w-4 h-4 text-indigo-500"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                      </svg>
+                      <svg
+                        fill="currentColor"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        class="w-4 h-4 text-indigo-500"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                      </svg>
                     </span>
-                    <span class="text-gray-500 text-sm">UI DEVELOPER</span>
+                    <span class="title-font font-medium text-gray-900">
+                      Tarun Barapatre
+                    </span>
+                    {/* <span class="text-gray-500 text-sm">UI DEVELOPER</span> */}
                   </span>
                 </a>
               </div>
@@ -46,22 +119,82 @@ const Testimonial = () => {
                   <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
                 </svg>
                 <p class="leading-relaxed mb-6">
-                  Synth chartreuse iPhone lomo cray raw denim brunch everyday
-                  carry neutra before they sold out fixie microdosing. Tacos
-                  pinterest fanny pack venmo, post-ironic heirloom try-hard
-                  pabst authentic iceland.
+                  Its been a nice experience with treatment plan with affordable
+                  price. I would suggest to everyone to visit and consult Dr.
+                  Surbhi Pande for long relief for dental problems.
                 </p>
                 <a class="inline-flex items-center">
-                  {/* <img
-                    alt="testimonial"
-                    src="https://dummyimage.com/107x107"
-                    class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"
-                  /> */}
+                  {/* <img alt="testimonial" src="https://dummyimage.com/107x107" class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"> */}
+                  <Image
+                    src="/human.svg"
+                    alt="Picture of the author"
+                    width={500}
+                    height={500}
+                    className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"
+                  />
+
                   <span class="flex-grow flex flex-col pl-4">
-                    <span class="title-font font-medium text-gray-900">
-                      Alper Kamu
+                    <span class="flex items-center">
+                      <svg
+                        fill="currentColor"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        class="w-4 h-4 text-indigo-500"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                      </svg>
+                      <svg
+                        fill="currentColor"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        class="w-4 h-4 text-indigo-500"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                      </svg>
+                      <svg
+                        fill="currentColor"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        class="w-4 h-4 text-indigo-500"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                      </svg>
+                      <svg
+                        fill="currentColor"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        class="w-4 h-4 text-indigo-500"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                      </svg>
+                      <svg
+                        fill="currentColor"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        class="w-4 h-4 text-indigo-500"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                      </svg>
                     </span>
-                    <span class="text-gray-500 text-sm">DESIGNER</span>
+                    <span class="title-font font-medium text-gray-900">
+                      Rumesh Patil
+                    </span>
+                    {/* <span class="text-gray-500 text-sm">DESIGNER</span> */}
                   </span>
                 </a>
               </div>
